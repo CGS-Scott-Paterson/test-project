bbe2a868ac1fbb11088fa3b626d1a0c748587942
Scott Paterson <42534@cgs.act.edu.au>
2015-07-24 12:51:32 +1000

    Inital commit

---
+++ TestProject/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by Scott Paterson on 24/07/2015.
+//  Copyright (c) 2015 Apress. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        println("Hello World")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
