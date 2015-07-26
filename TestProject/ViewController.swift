6f4bbcf4080d01f9d72d0ef8bcdecfd8e2628162
Scott Paterson <42534@cgs.act.edu.au>
2015-07-27 09:29:01 +1000

    Implemented awsome feature

--- TestProject/ViewController.swift
+++ TestProject/ViewController.swift
@@ -19,11 +19,12 @@ class ViewController: UIViewController {
         
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func awesomeFeature() {
+        
+        println("this is an awsome feature")
+    
+    
     }
-
 
 }
 
